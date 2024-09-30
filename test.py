941d498f52563d91253e2ed6db024f26360a4086
patrick437 <patrickgnoonan@gmail.com>
2024-09-30 12:51:56 +0100

    Adding different pre trained models to repos so can be tested on raspberry pi

--- test.py
+++ test.py
@@ -5,6 +5,10 @@ model = YOLO("yolov8n.yaml")
 
 # Load a pretrained YOLO model (recommended for training)
 model = YOLO("yolov8n.pt")
+model = YOLO("yolov8s.pt")
+model = YOLO("yolov8m.pt")
+model = YOLO("yolov8l.pt")
+model = YOLO("yolov8x.pt")
 
 # Train the model using the 'coco8.yaml' dataset for 3 epochs
 results = model.train(data="coco8.yaml", epochs=3)
